4af747981791cb2a1167c1d90b560ca5d3b85a8a
Ariel Wu <arielywt@atomicmail.io>
2026-05-08 11:22:59 -0400

    actualizacion

--- ejercicios.js
+++ ejercicios.js
@@ -6,7 +6,6 @@ let apellido = "Wu";
 console.log(nombre,apellido);
 
 // 2 Crear Numeros
-
 let valor = 5;
 
 valor += 5;
@@ -14,7 +13,6 @@ valor = valor*3;
 console.log("El resultado es: ",valor);
 
 // 3 Comida favorita
-
 comida="Spaguetti a la bolognesa";
 
 console.log("Mi comida favorita es:", comida);
@@ -29,7 +27,6 @@ console.log("Multiplicación:", a * b);
 console.log("Módulo:", a % b);
 
 // Cambiando a strings
-
 a = "10";
 b = "3";
 
@@ -39,7 +36,6 @@ console.log("Multiplicación con strings:", a * b);
 console.log("Módulo con strings:", a % b);
 
 // String y number
-
 a = "10";
 b = 3;
 
@@ -48,7 +44,6 @@ console.log("Resta string - number:", a - b);
 
 
 // 5 Comparaciones de edad
-
 let edad = 15;
 
 console.log("¿Es mayor a 10?", edad > 10);
@@ -56,28 +51,24 @@ console.log("¿Es menor a 18?", edad < 18);
 
 
 // 6 Uso de AND (&&)
-
 let numero = 8;
 
 console.log("¿Es mayor que 5 y menor que 10?", numero > 5 && numero < 10);
 
 
 // 7 Comparación de igualdad
-
 let nota = 6;
 
 console.log("¿La nota es igual a 7?", nota === 7);
 
 
 // 8 Comparación con temperatura
-
 let temperatura = 25;
 
 console.log("¿La temperatura es mayor o igual a 30?", temperatura >= 30);
 
 
 // 9 Comparar dos números
-
 let num1 = 20;
 let num2 = 15;
 
@@ -87,15 +78,13 @@ console.log("¿número 2 es mayor que número 1?", num2 > num1);
 
 
 // 10 Puede conducir
-
-edad = 17;
+edad = 17; //aquí evite el let porque ya tenía un let edad definido
 let tienePermiso = true;
 
 console.log("¿Puede conducir?", edad >= 18 || tienePermiso);
 
 
 // 11 Usuario y contraseña
-
 let usuario = "admin";
 let password = "1234";
 
